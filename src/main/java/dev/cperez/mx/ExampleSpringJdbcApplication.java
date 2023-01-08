662493c8fff8adb99702bc3c6997115afc904ad3
Cesar Perez <soycesar20k18@gmail.com>
2023-01-08 02:22:22 -0600

    first commit

---
+++ src/main/java/dev/cperez/mx/ExampleSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package dev.cperez.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cperez/mx/ExampleSpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cperez.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
